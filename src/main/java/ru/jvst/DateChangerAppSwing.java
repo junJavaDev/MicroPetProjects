feb155d542aad9401163992a17eff67820793269
junJavaDev <ogarkov.pro@gmail.com>
2023-11-25 04:51:19 +0300

    change logic to update update++ buttons

--- src/main/java/ru/jvst/DateChangerAppSwing.java
+++ src/main/java/ru/jvst/DateChangerAppSwing.java
@@ -1,9 +1,8 @@
 package ru.jvst;
 
 import javax.swing.*;
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
@@ -17,12 +16,18 @@ public class DateChangerAppSwing {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel datePanel = new JPanel();
+        datePanel.setPreferredSize(new Dimension(420, 40));
 
         Integer[] years = {2020, 2021, 2022, 2023, 2024};
         JComboBox<Integer> yearDropdown = new JComboBox<>(years);
+        yearDropdown.setPreferredSize(new Dimension(80, 30));
+
         Integer[] months = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
         JComboBox<Integer> monthDropdown = new JComboBox<>(months);
+        monthDropdown.setPreferredSize(new Dimension(80, 30));
+
         JComboBox<Integer> dayDropdown = new JComboBox<>();
+        dayDropdown.setPreferredSize(new Dimension(80, 30));
 
         updateDayDropdown(yearDropdown, monthDropdown, dayDropdown);
 
@@ -31,53 +36,80 @@ public class DateChangerAppSwing {
         datePanel.add(dayDropdown);
 
         JPanel buttonPanel = new JPanel();
-        JButton previousDayButton = new JButton("Предыдущий день");
-        JButton nextDayButton = new JButton("Следующий день");
-        JButton executeButton = new JButton("Выполнить");
-
-        buttonPanel.add(previousDayButton);
-        buttonPanel.add(nextDayButton);
+        buttonPanel.setPreferredSize(new Dimension(420, 40));
+        JButton executeButton = new JButton("Обновить");
+        executeButton.setPreferredSize(new Dimension(120, 30));
+        JButton executeNextButton = new JButton("Обновить++");
+        executeNextButton.setPreferredSize(new Dimension(120, 30));
         buttonPanel.add(executeButton);
+        buttonPanel.add(executeNextButton);
 
-        executeButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                int year = (int) yearDropdown.getSelectedItem();
-                int month = (int) monthDropdown.getSelectedItem();
-                int day = (int) dayDropdown.getSelectedItem();
-                String date = String.format("%d.%02d.%02d", year, month, day);
+        // Чтение даты из файла "LastCommit.txt" при запуске приложения
+        try (BufferedReader br = new BufferedReader(new FileReader("LastCommit.txt"))) {
+            String lastCommitDate = br.readLine();
+            LocalDate lastDate = LocalDate.parse(lastCommitDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            yearDropdown.setSelectedItem(lastDate.getYear());
+            monthDropdown.setSelectedItem(lastDate.getMonthValue());
+            dayDropdown.setSelectedItem(lastDate.getDayOfMonth());
+        } catch (IOException | DateTimeParseException e) {
+            // Обработка ошибок чтения файла или парсинга даты
+            e.printStackTrace();
+        }
 
-                DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-                String time = timeFormatter.format(LocalDateTime.now());
+        executeButton.addActionListener(e -> {
+            int year = (int) yearDropdown.getSelectedItem();
+            int month = (int) monthDropdown.getSelectedItem();
+            int day = (int) dayDropdown.getSelectedItem();
+            String date = String.format("%d-%02d-%02d", year, month, day);
 
-                String command = String.format("git commit --amend --no-edit --date=\"%s %s\"", date, time);
-                executeCommand(command);
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            String time = timeFormatter.format(LocalDateTime.now());
+
+            String command = String.format("git commit --amend --no-edit --date=\"%s %s\"", date, time);
+            executeCommand(command);
+
+            // Запись новой даты в файл "LastCommit.txt"
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter("LastCommit.txt"))) {
+                bw.write(date);
+            } catch (IOException ex) {
+                // Обработка ошибок записи в файл
+                ex.printStackTrace();
             }
         });
 
-        previousDayButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                LocalDate currentDate = getSelectedDate(yearDropdown, monthDropdown, dayDropdown);
-                LocalDate previousDay = currentDate.minus(1, ChronoUnit.DAYS);
+        executeNextButton.addActionListener(e -> {
+            int year = (int) yearDropdown.getSelectedItem();
+            int month = (int) monthDropdown.getSelectedItem();
+            int day = (int) dayDropdown.getSelectedItem();
+            String date = String.format("%d-%02d-%02d", year, month, day);
 
-                updateDropdowns(yearDropdown, monthDropdown, dayDropdown, previousDay);
-            }
-        });
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            String time = timeFormatter.format(LocalDateTime.now());
 
-        nextDayButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                LocalDate currentDate = getSelectedDate(yearDropdown, monthDropdown, dayDropdown);
-                LocalDate nextDay = currentDate.plus(1, ChronoUnit.DAYS);
+            String command = String.format("git commit --amend --no-edit --date=\"%s %s\"", date, time);
+            executeCommand(command);
 
-                updateDropdowns(yearDropdown, monthDropdown, dayDropdown, nextDay);
+            LocalDate currentDate = getSelectedDate(yearDropdown, monthDropdown, dayDropdown);
+            LocalDate nextDay = currentDate.plus(1, ChronoUnit.DAYS);
+
+            updateDropdowns(yearDropdown, monthDropdown, dayDropdown, nextDay);
+
+            // Запись новой даты в файл "LastCommit.txt"
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter("LastCommit.txt"))) {
+                bw.write(date);
+            } catch (IOException ex) {
+                // Обработка ошибок записи в файл
+                ex.printStackTrace();
             }
         });
 
         frame.add(datePanel, BorderLayout.NORTH);
         frame.add(buttonPanel, BorderLayout.CENTER);
 
-        frame.pack();
+        frame.setSize(420, 120);
         frame.setVisible(true);
     }
+
 
     private static void updateDropdowns(JComboBox<Integer> yearDropdown, JComboBox<Integer> monthDropdown, JComboBox<Integer> dayDropdown, LocalDate date) {
         yearDropdown.setSelectedItem(date.getYear());
