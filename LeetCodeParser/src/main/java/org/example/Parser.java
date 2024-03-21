f3b7e2a0a2e86613d68caaffec1419dae6081285
Alexander Ogarkov <ogarkov.pro@gmail.com>
2024-03-22 00:39:55 +0300

    fix

--- LeetCodeParser/src/main/java/org/example/Parser.java
+++ LeetCodeParser/src/main/java/org/example/Parser.java
@@ -1,38 +1,57 @@
 package org.example;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Objects;
 
 public class Parser {
 
     private static final String DESCRIPTION_SELECTOR = "div[data-track-load=description_content]";
     private static final String LINK_SELECTOR = "a.no-underline.truncate.cursor-text.whitespace-normal";
-    private static final String CHROME_OPTIONS = "--headless";
+    private static final String HEADLESS_UI_SELECTOR = "//button[text()='C++']";
+    private static final String JAVA_PUBLIC_SELECTOR = "//span[@class='mtk4' and text()='public']";
+    private static final String JAVA_DIV_SELECTOR = "//div[text()='Java']";
+    private static final String CODE_SELECTOR = "//*[@id=\"editor\"]/div[2]/div[1]/div/div/div[1]/div[2]/div[1]/div[5]";
+//    private static final String[] CHROME_OPTIONS = new String[]{"headless"};
 
     public static String[] parse(String fullUrl) throws InterruptedException {
         String clazz;
         String commit;
-        WebDriverManager.chromedriver().setup();
 
         // ЗАПУСК
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments(CHROME_OPTIONS);
         WebDriver driver = new ChromeDriver();
+//        WebDriver driver = new ChromeDriver(new ChromeOptions().addArguments(CHROME_OPTIONS));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
         String url = trimUrl(fullUrl);
         driver.get(url);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        System.out.println(driver.getPageSource());
         // ССЫЛКА
         WebElement linkElement = waitUntilAndGetByCssSelector(driver, wait, LINK_SELECTOR);
-        String linkText = linkElement.getText();
+        WebElement headlessUi = waitUntilAndGetByXpath(driver, wait, HEADLESS_UI_SELECTOR);
+        if (headlessUi != null) {
+            headlessUi.click();
+        }
+        WebElement javaDiv = waitUntilAndGetByXpath(driver, wait, JAVA_DIV_SELECTOR);
+        if (javaDiv != null) {
+            javaDiv.click();
+        }
 
+        waitUntilAndGetByXpath(driver, wait, JAVA_PUBLIC_SELECTOR);
+        String fullCode = Objects.requireNonNull(waitUntilAndGetByXpath(driver, wait, CODE_SELECTOR)).getText();
+
+
+        String code = fullCode.substring(17, fullCode.length()-16);
+
+        assert linkElement != null;
+        String linkText = linkElement.getText();
         // ИМЯ КЛАССА
         String className = convertToValidClassName(linkText);
 
@@ -41,8 +60,8 @@ public class Parser {
         description += driver
                 .findElement(By.cssSelector(DESCRIPTION_SELECTOR))
                 .getAttribute("outerHTML");
-        clazz = String.format("package ru.jvst.leetcode;\n\n/**\n%s\n**/\n\nclass %s {\n    \n}",
-                description, className);
+        clazz = String.format("package ru.jvst.leetcode;\n\n/**\n%s\n**/\n\nclass %s {\n%s\n        \n    }\n}",
+                description, className,code);
 
 
         // КОММИТ
